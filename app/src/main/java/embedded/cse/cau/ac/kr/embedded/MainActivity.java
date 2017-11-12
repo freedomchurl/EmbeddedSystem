51146599028a130c4ac94020b6d152ca25e5eb4b
freedomchurl <freedomchurl@naver.com>
2017-11-12 18:00:25 +0900

    Init Push

---
+++ app/src/main/java/embedded/cse/cau/ac/kr/embedded/MainActivity.java
@@ -0,0 +1,16 @@
+package embedded.cse.cau.ac.kr.embedded;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/embedded/cse/cau/ac/kr/embedded/SingleActivity.java
@@ -0,0 +1,27 @@
+package embedded.cse.cau.ac.kr.embedded;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+
+/**
+ * Created by caucse on 2017-11-08.
+ */
+
+public class SingleActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_activity);
+    }
+
+
+
+
+
+}
+
